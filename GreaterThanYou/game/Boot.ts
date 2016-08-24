3fe92529f4eaafe706f994f7a7fdec6f2702a3b7
Bobbyberta <bobbyberta@googlemail.com>
2016-08-24 11:23:11 +0100

    Rotated Image and code

--- GreaterThanYou/game/Boot.ts
+++ GreaterThanYou/game/Boot.ts
@@ -6,6 +6,7 @@ GreaterThan.Boot = function (game) {
 GreaterThan.Boot.prototype = {
 
     preload: function () {
+        game.load.image('rotate', 'assets3/rotate.png');
         //should probably add some nice graphics to tell you to change the rotation of the device if it is wrong
         //and graphics of the loading bar for the preload state
     },
@@ -18,12 +19,12 @@ GreaterThan.Boot.prototype = {
             this.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
             document.body.style.backgroundSize = "" + game.scale.width * 3.1796875 + "px 100%";
             document.body.style.backgroundPosition = "center"
-        // } else {
-        //     this.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
-        //     this.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
-            // this.scale.forceOrientation(false, true);
-            // this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
-            // this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
+        } else {
+            this.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
+            this.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
+            this.scale.forceOrientation(false, true);
+            this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
+            this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
         }
         this.scale.pageAlignHorizontally = true;
         //this.scale.pageAlignVertically = true;
@@ -33,26 +34,26 @@ GreaterThan.Boot.prototype = {
 
     },
 
-    // ////Code from Mohameds Tangled Web Demo for screen rotation
-    // enterIncorrectOrientation: function () {
-    //     if (!needToTurn) {
-    //         needToTurn = true;
-    //         game.input.maxPointers = 0;
-    //         if (rotate != null && rotate != undefined)rotate.destroy();
-    //         rotate = game.add.sprite(0, game.camera.y, "rotate");
-    //         game.world.bringToTop(rotate);
-    //         game.physics.arcade.isPaused = true
-    //     }
-    // },
-    //
-    // leaveIncorrectOrientation: function () {
-    //     if (needToTurn) {
-    //         if (!gamePaused)
-    //             game.physics.arcade.isPaused = false;
-    //     }
-    //     needToTurn = false;
-    //     game.input.maxPointers = 1;
-    //     rotate.destroy()
-    // }
+    ////Code from Mohameds Tangled Web Demo for screen rotation
+    enterIncorrectOrientation: function () {
+        if (!needToTurn) {
+            needToTurn = true;
+            game.input.maxPointers = 0;
+            if (rotate != null && rotate != undefined)rotate.destroy();
+            rotate = game.add.sprite(0, game.camera.y, "rotate");
+            game.world.bringToTop(rotate);
+            game.physics.arcade.isPaused = true
+        }
+    },
+
+    leaveIncorrectOrientation: function () {
+        if (needToTurn) {
+            if (!gamePaused)
+                game.physics.arcade.isPaused = false;
+        }
+        needToTurn = false;
+        game.input.maxPointers = 1;
+        rotate.destroy()
+    }
 
 };
