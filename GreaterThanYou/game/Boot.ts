62c6d0c0f72af937ef454234e7f645406d8b87c5
Bobbyberta <bobbyberta@googlemail.com>
2016-08-24 11:37:52 +0100

    un commented code

--- GreaterThanYou/game/Boot.ts
+++ GreaterThanYou/game/Boot.ts
@@ -27,7 +27,7 @@ GreaterThan.Boot.prototype = {
             this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
         }
         this.scale.pageAlignHorizontally = true;
-        //this.scale.pageAlignVertically = true;
+        this.scale.pageAlignVertically = true;
         
         
         this.game.state.start("preload")
